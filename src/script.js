5b36c1609cf865804a91298a540fee541cbff3d0
Leozinho2831 <leoferraz1818@gmail.com>
2024-11-20 13:53:32 -0300

    validation date in html

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
 dayjs.locale('pt-br');
+
+const inputDate = document.querySelector('input[type="date"]');
+
+// tem que ser no formato americano por culpa do html
+const today = dayjs().format('YYYY-MM-DD');
+
+inputDate.min = today;
