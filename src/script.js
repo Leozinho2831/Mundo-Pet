f6f9f5b7ddf156dddbc9e422b4109d41cd3c006a
Leozinho2831 <leoferraz1818@gmail.com>
2024-11-20 22:25:13 -0300

    new formatation inputs texts to date style e hour style

--- src/script.js
+++ src/script.js
@@ -1,13 +1,6 @@
 // deixando data formato brasileiro
 dayjs.locale('pt-br');
 
-const inputDate = document.querySelector('input[type="date"]');
-
-// tem que ser no formato americano por culpa do html
-const today = dayjs().format('YYYY-MM-DD');
-
-inputDate.min = today;
-
 // formatando input tel
 const telInput = document.querySelector('input[type="tel"]');
 
@@ -16,8 +9,7 @@ function formatInputTel(){
     const clearLetters = telInput.value.replace(/\D/g, '').substring(0,11);
     telInput.value = clearLetters;
 
-    const numberArray = clearLetters.split('');
-    
+    const numberArray = telInput.value.split('');
     let numberFormatted = '';
 
     if(numberArray.length > 0){
@@ -38,15 +30,52 @@ function formatInputTel(){
 
 telInput.addEventListener('input', formatInputTel);
 
-// formatando input de data e hora
+// formatando input de data
 const dateInput = document.querySelector('.js-inputDate');
 
 function formatInputDate(){
+    const clearLetters = dateInput.value.replace(/\D/g, '').substring(0,8);
+    dateInput.value = clearLetters;
 
+    const dateArray = dateInput.value.split('');
+    let dateFormatted = '';
+
+    if(dateArray.length > 0){
+        dateFormatted += `${dateArray.slice(0,2).join('')}`;
+    }
+
+    if(dateArray.length > 2){
+        dateFormatted += `/${dateArray.slice(2,4).join('')}`;
+    }
+
+    if(dateArray.length > 4){
+        dateFormatted += `/${dateArray.slice(4,8).join('')}`;
+    }
+
+    dateInput.value = dateFormatted;
 }
 
+dateInput.addEventListener('input', formatInputDate);
+
+// formatando input de horas
 const hourInput = document.querySelector('.js-inputHour');
 
 function formatInputHour(){
+    const clearLetters = hourInput.value.replace(/\D/g, '').substring(0,4);
+    hourInput.value = clearLetters;
 
+    const hourArray = hourInput.value.split('');
+    let hourFormatted = '';
+
+    if(hourArray.length > 0){
+        hourFormatted += `${hourArray.slice(0,2).join('')}`;
+    }
+
+    if(hourArray.length > 2){
+        hourFormatted += `:${hourArray.slice(2,4).join('')}`;
+    }
+
+    hourInput.value = hourFormatted;
 }
+
+hourInput.addEventListener('input', formatInputHour);
