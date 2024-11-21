b325b00a268da1c36175b602a186fb9560aa0941
Leozinho2831 <leoferraz1818@gmail.com>
2024-11-20 21:53:22 -0300

    formatted input tel

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+// deixando data formato brasileiro
 dayjs.locale('pt-br');
 
 const inputDate = document.querySelector('input[type="date"]');
@@ -6,3 +7,32 @@ const inputDate = document.querySelector('input[type="date"]');
 const today = dayjs().format('YYYY-MM-DD');
 
 inputDate.min = today;
+
+const telInput = document.querySelector('input[type="tel"]');
+
+function formatInputTel(){
+    // não permitir letras
+    const clearLetters = telInput.value.replace(/\D/g, '').substring(0,11);
+    telInput.value = clearLetters;
+
+    const numberArray = clearLetters.split('');
+    
+    let numberFormatted = '';
+
+    if(numberArray.length > 0){
+        numberFormatted += `(${numberArray.slice(0,2).join('')})`;
+    }
+
+    if(numberArray.length > 2){
+        numberFormatted += ` ${numberArray.slice(2,7).join('')}`;
+    }
+
+    if(numberArray.length > 7){
+        numberFormatted += `-${numberArray.slice(7,11).join('')}`;
+    }
+
+    telInput.value = numberFormatted;
+    
+}
+
+telInput.addEventListener('input', formatInputTel);
