fdd6666e6f3ebfcc28a61ddf47970f3b956e763c
Leozinho2831 <leoferraz1818@gmail.com>
2025-03-15 21:56:37 -0300

    new name file javascript

---
+++ src/javascript/openForm.js
@@ -0,0 +1,35 @@
+dayjs.locale('pt-br');
+
+const buttonForm = document.querySelector('.js-buttonForm');
+const form = document.querySelector('.js-form');
+const main = document.querySelector('.js-main');
+
+const sections = document.querySelectorAll('.js-blur');
+
+buttonForm.onclick  = () => {
+    form.classList.remove('hidden');
+    form.classList.add('grid');
+        
+    sections.forEach((section) => {
+        section.classList.add('blur-sm');
+    });
+}
+
+main.onclick = (event) => {
+
+    if(form.classList.contains('grid')){
+            
+    const itemClick = event.target;
+            
+    if(!form.contains(itemClick)){
+        form.classList.remove('grid');
+        form.classList.add('hidden');
+
+            sections.forEach((section) => {
+                section.classList.remove('blur-sm');
+            });
+        }
+
+    }
+
+}
